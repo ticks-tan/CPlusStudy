1d481e7a4e5b010f0aa41a2f5d4f7d5c2d40f5bf
ticks <2938384958@qq.com>
2022-05-17 17:34:38 +0800

    init CMakeLists.txt

---
+++ Plus11/main.cc
@@ -0,0 +1,19 @@
+/*
+*
+* Copyright(C) 2022 MiracleForest Studio. All Rights Reserved.
+*
+* @filename:main.cc
+* @creation time:2022.05.17 16:51
+* @created by:
+* -----------------------------------------------------------------------------
+*
+* -----------------------------------------------------------------------------
+* If you have contact or find bugs,
+* you can go to Github or email (MiracleForest@Outlook.com) to give feedback.
+* We will try to do our best!
+*/
+
+int main()
+{
+    return 0;
+}
